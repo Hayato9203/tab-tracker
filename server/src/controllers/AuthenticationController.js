7334b93fed95f2e1b0f5128fb3f04e8f74d49095
hayato <hayato9203@gmail.com>
2018-04-12 22:36:13 +0800

    Use joi module to verify the validity of registration information

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,14 @@
+const {User} = require('../models')
+
 module.exports = {
-  register (req, res) {
-    res.send({
-      message: `Hello ${req.body.email}! Your user was registered, Have fun!`
-    })
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJson())
+    } catch (err) {
+      res.status(400).send({
+        error: `This email account is already is use.`
+      })
+    }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,10 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+// 用作于检查body的中间件
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
-  app.post('/register', AuthenticationController.register)
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
 }
