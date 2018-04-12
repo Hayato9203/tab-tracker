3dd0e17e39e260dfbd91f74c692c459b085e0977
hayato <hayato9203@gmail.com>
2018-04-12 17:18:02 +0800

    Intro

---
+++ client/src/services/AuthenticationService.js
@@ -0,0 +1,15 @@
+import Api from '@/services/Api'
+
+export default {
+  // register函数接收post请求的credentials(证书)参数
+  register (credentials) {
+    return Api().post('register', credentials)
+  }
+}
+
+// AuthenticationService.register({
+//     email:`testing@gmail.com`,
+//     passworf:`123456`
+// })
+
+// implement a UI to be to use for them this authentication service
