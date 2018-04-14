472f71e0859413dcc685fb2a64fa84860a5cb737
hayato <hayato9203@gmail.com>
2018-04-14 14:25:09 +0800

    Rendering pages using the VUETIFY module(1)

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <div class="error" v-html="error"></div>
-    <input v-model="email" type="email" name="email" placeholder="email">
-    <br>
-    <input v-model="password" type="password" name="password" placeholder="password">
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout row wrap>
+    <!-- 将12格分成6+6，内容总占6，左右分别缩进3格 -->
+    <v-flex xs6 offset-xs3>
+      <v-card class="white elevation-2">
+        <v-toolbar flat dense class="cyan">
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <v-card class="pl-4 pr-4 pt-3 pb-3">
+          <v-text-field label="Email" v-model="email"></v-text-field>
+          <br />
+          <v-text-field label="Password" v-model="password"></v-text-field>
+          <br />
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </v-card>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
