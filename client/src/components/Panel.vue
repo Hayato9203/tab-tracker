7959f772a0f607cf9ba4dd3f9f6d015b86203ac5
hayato <hayato9203@gmail.com>
2018-04-15 21:52:38 +0800

    Add Songs page2(panel)

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card class="white elevation-2">
+    <v-toolbar dark flat dense class="cyan">
+        <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
+    <v-card class="pl-4 pr-4 pt-3 pb-3">
+        <slot>No slot defined.</slot>
+    </v-card>
+    </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'The Songs'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,21 +1,17 @@
 <template>
   <v-layout row wrap>
     <v-flex xs6 offset-xs3>
-      <v-card class="white elevation-2">
-        <v-toolbar dark flat dense class="cyan">
-          <v-toolbar-title>Songs</v-toolbar-title>
-        </v-toolbar>
-        <v-card class="pl-4 pr-4 pt-3 pb-3">
-
-        </v-card>
-      </v-card>
+        <panel />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 export default {
-
+  components: {
+    Panel
+  }
 }
 </script>
 
