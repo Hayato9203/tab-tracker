ef4288f081d269af8aa7ceafae0819645d143ca4
hayato <hayato9203@gmail.com>
2018-04-15 23:12:06 +0800

    Song: client adds songs route, server adds songs data model and routes

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,11 @@
   <v-layout row wrap>
     <v-flex xs6 offset-xs3>
         <panel title="Songs">
+            <div v-for="song in songs" :key="song.title">
+                {{song.title}} -
+                {{song.album}} -
+                {{song.artist}}
+            </div>
         </panel>
     </v-flex>
   </v-layout>
@@ -9,7 +14,25 @@
 
 <script>
 import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
+
 export default {
+  data () {
+    return {
+      songs: [
+        {title: 'うたかた花火', album: '星が瞬くこんな夜に', artist: 'supercell'},
+        {title: 'ビードロ模様', album: 'ビードロ模様', artist: 'やなぎなぎ'},
+        {title: '夏の終わりの雨音が', album: 'デュオトリオコレクション VOL.1 ~SUMMER VACATION~', artist: '伊波杏樹;諏訪ななか'},
+        {title: '夜空はなんでも知ってるの？', album: '元気全開DAY！DAY！DAY！', artist: 'CYaRon'},
+        {title: 'アクアテラリウム', album: 'アクアテラリウム', artist: 'やなぎなぎ'},
+        {title: 'Letter Song', album: 'Letter Song', artist: 'ヲタみん'}
+      ]
+    }
+  },
+  async mounted () {
+    // 从后台取得所有歌曲
+    this.songs = await SongsService.index()
+  },
   components: {
     Panel
   }
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,7 @@ const AuthenticationController = require('./controllers/AuthenticationController
 
 // 用作于检查body的中间件
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
 
 module.exports = (app) => {
   app.post('/register',
@@ -9,4 +10,8 @@ module.exports = (app) => {
     AuthenticationController.register)
   app.post('/login',
     AuthenticationController.login)
+  app.get('/songs',
+    SongsController.index)
+  app.post('/songs',
+    SongsController.post)
 }
