01d95abf499fac91fa20d2b93043b119ff780d67
hayato <hayato9203@gmail.com>
2018-04-15 21:07:36 +0800

    Use vuex to save the user s authentication status

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -32,10 +32,13 @@ export default {
   methods: {
     async login () {
       try {
-        await AuthenticationService.login({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
+        // 调用store的setToken方法,将返回的jwt的token保存到store中
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
