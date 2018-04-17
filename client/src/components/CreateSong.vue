bc54810a6dbcd6e9617b65f5bfa610ac80728ee6
hayato <hayato9203@gmail.com>
2018-04-17 22:04:01 +0800

    Add a warning to warn fill all form field

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -7,19 +7,19 @@
                     <v-text-field label="Title" v-model="song.title" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Artist" v-model="song.artist"></v-text-field>
+                    <v-text-field label="Artist" v-model="song.artist" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Genre" v-model="song.genre"></v-text-field>
+                    <v-text-field label="Genre" v-model="song.genre" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Album" v-model="song.album"></v-text-field>
+                    <v-text-field label="Album" v-model="song.album" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Album Image Url" v-model="song.albumImageUrl"></v-text-field>
+                    <v-text-field label="Album Image Url" v-model="song.albumImageUrl" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Youtube Id" v-model="song.youtubeId"></v-text-field>
+                    <v-text-field label="Youtube Id" v-model="song.youtubeId" required :rules="[required]"></v-text-field>
                 </v-card>
             </panel>
         </v-card>
@@ -28,13 +28,14 @@
         <v-card class="white elevation-2">
             <panel title="Song Structure">
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Tab" multi-line v-model="song.tab"></v-text-field>
+                    <v-text-field label="Tab" multi-line v-model="song.tab" required :rules="[required]"></v-text-field>
                 </v-card>
                 <v-card class="pl-4 pr-4 pt-3 pb-3">
-                    <v-text-field label="Lyrics" multi-line v-model="song.lyrics"></v-text-field>
+                    <v-text-field label="Lyrics" multi-line v-model="song.lyrics" required :rules="[required]"></v-text-field>
                 </v-card>
             </panel>
         </v-card>
+        <div class="error" v-if="error">{{error}}</div>
         <v-btn dark class="cyan" @click="create">Create the Song</v-btn>
     </v-flex>
 </v-layout>
@@ -56,6 +57,7 @@ export default {
         lyrics: null,
         tab: null
       },
+      error: null,
       required: (value) => !!value || 'Required.'
     }
   },
@@ -64,6 +66,14 @@ export default {
   },
   methods: {
     async create () {
+      this.error = null
+      const areAllFieldsFilledIn = Object
+        .keys(this.song)
+        .every(key => !!this.song[key])
+      if (!areAllFieldsFilledIn) {
+        this.error = `Please fill in all the required fields.`
+        return
+      }
       // call API
       try {
         await SongsService.post(this.song)
