fa3478ecdd9bce78958f0c4fff46c3f6bc8e33e7
hayato <hayato9203@gmail.com>
2018-04-18 11:51:34 +0800

    get song details from the back-end with /songs/:songId on the ViewSong page(2) - show song(2)

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,9 +1,31 @@
 <template>
-  <panel title="Song Metadata">
-    {{song.title}}
-    {{song.artist}}
-    {{song.genre}}
-  </panel>
+  <v-layout>
+    <v-flex xs6>
+      <panel title="Song Metadata">
+        <v-layout class="song">
+          <v-flex xs6>
+            <div class="song-title">{{song.title}}</div>
+            <div class="song-artist">{{song.artist}}</div>
+            <div class="song-genre">{{song.genre}}</div>
+            <!-- 定义v-on:click中带有参数的route -->
+            <v-btn
+              dark class="cyan"
+              @click="navigateTo({
+                name: 'song',
+                params: {songId: song.id}
+              })">
+              View this song
+            </v-btn>
+          </v-flex>
+          <v-flex xs6>
+            <img :src="song.albumImageUrl" class="album-image">
+          </v-flex>
+        </v-layout>
+      </panel>
+    </v-flex>
+    <v-flex xs6>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -30,5 +52,22 @@ export default {
 </script>
 
 <style scoped>
-
+.song{
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title{
+  font-size: 30px;
+}
+.song-artist{
+  font-size: 24px;
+}
+.song-genre{
+  font-size: 18px;
+}
+.album-image{
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
