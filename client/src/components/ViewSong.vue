424483fdb194ba77a392de734b1a6f990ab86ed4
hayato <hayato9203@gmail.com>
2018-04-18 11:45:05 +0800

    get song details from the back-end with /songs/:songId on the ViewSong page(2) - show song

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-
-    </div>
+  <panel title="Song Metadata">
+    {{song.title}}
+    {{song.artist}}
+    {{song.genre}}
+  </panel>
 </template>
 
 <script>
 import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+
 export default {
   data () {
     return {
-      song: null
+      song: {}
     }
   },
   async mounted () {
     // 从vuex中获取songId
     const songId = this.$store.state.route.params.songId
     // console.log(songId)
-    this.song = await SongsService.show(songId)
+    this.song = (await SongsService.show(songId)).data
     console.log(this.song)
+  },
+  components: {
+    Panel
   }
 }
 </script>
