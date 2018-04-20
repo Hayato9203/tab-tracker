0be0bf2aec50090d68da0d20468ac5ec57d61248
hayato <hayato9203@gmail.com>
2018-04-20 23:23:12 +0800

    vuex-persistedstate: Vuex.Store would not refresh

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
+// vuex-persistedstate当你刷新页面时，下面定义的Vuex.Store所有状态(登录)都会被本地保存而不会重新载入
 export default new Vuex.Store({
   strict: true,
+  plugins: [createPersistedState()],
   state: {
     token: null,
     user: null,
