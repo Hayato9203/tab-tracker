b692f3176eb33cdb8b7913644c1f4bd897fb1195
hayato <hayato9203@gmail.com>
2018-04-22 01:07:58 +0800

    use passport&passport-jwt(2) - add Authorization token to headers

--- server/src/policies/isAuthenticated.js
+++ server/src/policies/isAuthenticated.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 // jwt从../passport.js中传来的JwtStrategy
 module.exports = function (req, res, next) {
   passport.authenticate('jwt', function (err, user) {
-    if (err) {
+    if (err || !user) {
       res.status(400).send({
         error: `you do not have acces to this resource`
       })
